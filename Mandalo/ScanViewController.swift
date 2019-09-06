381fad03a06bb6d2e39f1dbe85822e50a113f918
Kevin Schaefer <kevin.schaefer@jambit.com>
2019-09-06 21:52:39 +0200

    Scanning QRCode works

---
+++ Mandalo/ScanViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ScanViewController.swift
+//  Mandalo
+//
+//  Created by Kevin Schaefer on 06.09.19.
+//  Copyright © 2019 Kevin Schaefer. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ScanViewController: UIViewController {
+
+    fileprivate var avCaptureSession: AVCaptureSession?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setUpCaptureSession()
+    }
+
+    fileprivate func setUpCaptureSession() {
+        let session = AVCaptureSession()
+        // We don't need to control audio and video output settings
+        session.sessionPreset = AVCaptureSession.Preset.inputPriority
+
+        // Get the current camera and from that derive an input
+        let camera = AVCaptureDevice.default(for: AVMediaType.video)
+        let input = try? AVCaptureDeviceInput(device: camera!)
+
+        guard input != nil else { return }
+
+        // We need still image and data output to show and analyze the images
+        let output = AVCapturePhotoOutput()
+        let dataOutput = AVCaptureVideoDataOutput()
+
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: .default))
+
+        guard session.canAddInput(input!) && session.canAddOutput(output) && session.canAddOutput(dataOutput) else { return }
+
+        // Add inputs and outputs to our session
+        session.addInput(input!)
+        session.addOutput(output)
+        session.addOutput(dataOutput)
+
+        // Determine the preview layer which will be shown to the user
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+
+        // We do not want the previewLayer to be resized, but rather want it to be cut
+        // so that only a given window is shown
+        previewLayer.frame = view.bounds
+        view.layer.masksToBounds = true
+
+        previewLayer.connection?.videoOrientation = .portrait
+
+        // Add the preview layer to our view and start the session
+        view.layer.addSublayer(previewLayer)
+        session.startRunning()
+        self.avCaptureSession = session
+    }
+}
+
+
+extension ScanViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let pixelBufferOptional = CMSampleBufferGetImageBuffer(sampleBuffer)
+
+        guard let pixelBuffer = pixelBufferOptional else { return }
+
+        let image = CIImage(cvPixelBuffer: pixelBuffer)
+        let context = CIContext()
+        let detector = CIDetector.init(ofType: CIDetectorTypeQRCode, context: context, options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+
+        let featuresOptional = detector?.features(in: image)
+
+        guard let features = featuresOptional, features.count > 0 else { return }
+
+        print((features.first as? CIQRCodeFeature)?.messageString)
+    }
+}
