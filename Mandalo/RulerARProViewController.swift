2a7ad8dcf7cb1717484db61fd1cf4584fc862453
Kevin Schaefer <kevin.schaefer@jambit.com>
2019-09-07 12:24:55 +0200

    Add RulerARProViewController

---
+++ Mandalo/RulerARProViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RulerARProViewController.swift
+//  Mandalo
+//
+//  Created by Kevin Schaefer on 07.09.19.
+//  Copyright © 2019 Kevin Schaefer. All rights reserved.
+//
+
+import UIKit
+
+class RulerARProViewController: UIViewController {
+    
+}
